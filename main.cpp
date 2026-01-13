20eebfa762b1209856d8f46edee5c3a8db52b616
Yazan <yazan.m.abualenain@gmail.com>
2026-01-13 18:11:19 +0300

    initalization

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <Louvre/LCompositor.h>
+
+
+
+using namespace Louvre;
+
+
+
+
+int main() {
+    std::cout << "Starting SmokeWM..." << std::endl;
+
+    LCompositor* compositor = new LCompositor();
+
+    if (!compositor->start()) {
+        std::cerr << "Failed to start." << std::endl;
+        delete compositor;
+        return 1;
+    }
+
+
+
+    while (compositor->state()==2) {
+        std::string line;
+        std::getline(std::cin, line);
+        if (line == "exit") {
+            break;
+        }
+    }
+
+
+
+    compositor->finish();
+    
+    std::cout << "Compositor finished cleanly." << std::endl;
+    delete compositor;
+
+    _Exit(0);
+}
